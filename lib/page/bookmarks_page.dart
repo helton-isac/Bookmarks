6f2b3c78c588d34febf63ba9d7bada1285e5f045
Helton Isac <helton.isac@gmail.com>
2020-07-07 14:06:21 -0300

    Creating another page to handle new bookmarks

--- lib/page/bookmarks_page.dart
+++ lib/page/bookmarks_page.dart
@@ -1,21 +1,29 @@
 import 'package:bookmarks/model/bookmark.dart';
+import 'package:bookmarks/page/add_bookmark_page.dart';
 import 'package:bookmarks/widget/bookmarks_list_widget.dart';
 import 'package:flutter/material.dart';
 
 class BookmarksPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     List<Bookmark> bookmarksList = [
       Bookmark("Flutter", "https://flutter.dev"),
       Bookmark("Google", "https://google.com"),
     ];
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text("My Bookmarks"),
-        ),
-        body: BookmarksListWidget(bookmarksList)
+      appBar: AppBar(
+        title: Text("My Bookmarks"),
+      ),
+      body: BookmarksListWidget(bookmarksList),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        icon: Icon(Icons.add),
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(builder: (context) => AddBookmarkPage()));
+        },
+        label: Text("Add"),
+      ),
     );
   }
 }
