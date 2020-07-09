a32f6bde96938d7f8e40adeb8bfbac450e8724bc
Helton Isac <helton.isac@gmail.com>
2020-07-09 15:45:08 -0300

    Starting the development of the add bookmark page

--- lib/page/add_bookmark_page.dart
+++ lib/page/add_bookmark_page.dart
@@ -4,7 +4,40 @@ class AddBookmarkPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Add a new bookmark"),),
-    );
+        appBar: AppBar(
+          title: Text("Add a new bookmark"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.check),
+          backgroundColor: Colors.green,
+          onPressed: () {
+            print("Input captured");
+          },
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextFormField(
+                autofocus: true,
+                decoration: InputDecoration(
+                    icon: Icon(Icons.title),
+                    labelText: "Title",
+                    hintText: "Title of the bookmark",
+                    border: OutlineInputBorder()),
+              ),
+              SizedBox(
+                height: 16.0,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                    icon: Icon(Icons.link),
+                    labelText: "URL",
+                    hintText: "Webpage link",
+                    border: OutlineInputBorder()),
+              )
+            ],
+          ),
+        ));
   }
 }
