d2d303ae07603fbbb89711c0c7438d4284227ea4
Helton Isac <helton.isac@gmail.com>
2020-07-09 15:58:46 -0300

    Controlling Focus and keyboard navigation

--- lib/page/add_bookmark_page.dart
+++ lib/page/add_bookmark_page.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 
-class AddBookmarkPage extends StatelessWidget {
+class AddBookmarkPage extends StatefulWidget {
+  @override
+  _AddBookmarkPageState createState() => _AddBookmarkPageState();
+}
+
+class _AddBookmarkPageState extends State<AddBookmarkPage> {
+  final _titleTexController = TextEditingController();
+
+  final _linkTexController = TextEditingController();
+
+  final _linkFocusNode = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,7 +22,8 @@ class AddBookmarkPage extends StatelessWidget {
           child: Icon(Icons.check),
           backgroundColor: Colors.green,
           onPressed: () {
-            print("Input captured");
+            print("Title:  ${_titleTexController.text}");
+            print("Link:  ${_linkTexController.text}");
           },
         ),
         body: Padding(
@@ -20,6 +32,11 @@ class AddBookmarkPage extends StatelessWidget {
             children: [
               TextFormField(
                 autofocus: true,
+                controller: _titleTexController,
+                textInputAction: TextInputAction.next,
+                onFieldSubmitted: (textInput) {
+                  FocusScope.of(context).requestFocus(_linkFocusNode);
+                },
                 decoration: InputDecoration(
                     icon: Icon(Icons.title),
                     labelText: "Title",
@@ -30,6 +47,8 @@ class AddBookmarkPage extends StatelessWidget {
                 height: 16.0,
               ),
               TextFormField(
+                controller: _linkTexController,
+                focusNode: _linkFocusNode,
                 decoration: InputDecoration(
                     icon: Icon(Icons.link),
                     labelText: "URL",
@@ -39,5 +58,13 @@ class AddBookmarkPage extends StatelessWidget {
             ],
           ),
         ));
+  }
+
+  @override
+  void dispose() {
+    _titleTexController.dispose();
+    _linkTexController.dispose();
+    _linkFocusNode.dispose();
+    super.dispose();
   }
 }
